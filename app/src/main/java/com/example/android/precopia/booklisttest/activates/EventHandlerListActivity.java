c1442333766ca904844e8d062da2b65f59b2ca1d
DavidPrecopia <35779726+DavidPrecopia@users.noreply.github.com>
2018-04-05 22:38:06 -0400

    Launcher Icon - Replaced default for practice

--- app/src/main/java/com/example/android/precopia/booklisttest/activates/EventHandlerListActivity.java
+++ app/src/main/java/com/example/android/precopia/booklisttest/activates/EventHandlerListActivity.java
@@ -4,6 +4,14 @@ import android.content.Intent;
 
 import com.example.android.precopia.booklisttest.book.Book;
 
+
+/**
+ * Instead of RecyclerView.Adapter declaring an Interface that is
+ * implemented by RecyclerView's enclosing Activity - I'm using data binding.
+ *
+ * In RecyclerView.ViewHolder I bind an instance of this class
+ * to an individual list item layout
+ */
 public final class EventHandlerListActivity {
 	
 	private ListActivity listActivity;
